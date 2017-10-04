11699bf39536f28ac872969ad3744ab674ed26e2
佐伯紘二 <koji@aiuchiko.local>
2017-10-04 22:32:40 +0900

    first commit

---
+++ src/main/java/org/enlightenseries/DomainDictionary/DomainDictionaryApplication.java
@@ -0,0 +1,12 @@
+package org.enlightenseries.DomainDictionary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainDictionaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DomainDictionaryApplication.class, args);
+	}
+}
