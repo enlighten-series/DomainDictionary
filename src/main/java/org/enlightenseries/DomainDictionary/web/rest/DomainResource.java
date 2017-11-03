b63eec9946bce8c160447cd2955123a92b79157a
佐伯紘二 <saikou9901@gmail.com>
2017-11-03 21:41:42 +0900

    ドメイン単品を返すAPIモック

--- src/main/java/org/enlightenseries/DomainDictionary/web/rest/DomainResource.java
+++ src/main/java/org/enlightenseries/DomainDictionary/web/rest/DomainResource.java
@@ -1,7 +1,9 @@
 package org.enlightenseries.DomainDictionary.web.rest;
 
 import org.enlightenseries.DomainDictionary.domain.Domain;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,13 +15,13 @@ import java.util.List;
 public class DomainResource {
 
   @GetMapping("/domains")
-  public List<Domain> getAll() {
+  public List<Domain> getAllDomains() {
     return Arrays.asList(
       new Domain(
         1L,
         "顧客番号",
         "123-456-789",
-        "おきゃくさんごとにつく",
+        "おきゃくさんごとにつくおきゃくさんごとにつくおきゃくさんごとにつくおきゃくさんごとにつくおきゃくさんごとにつくおきゃくさんごとにつくおきゃくさんごとにつく",
         "おもいつき"
       ),
       new Domain(
@@ -30,5 +32,34 @@ public class DomainResource {
         "いる！"
       )
     );
+  }
+
+  @GetMapping("/domains/{id}")
+  public ResponseEntity<Domain> getDomain(@PathVariable Long id) {
+    Domain domain = null;
+
+    if (id == 1L) {
+      domain = new Domain(
+        1L,
+        "顧客番号",
+        "123-456-789",
+        "おきゃくさんごとにつくおきゃくさんごとにつくおきゃくさんごとにつくおきゃくさんごとにつくおきゃくさんごとにつくおきゃくさんごとにつくおきゃくさんごとにつく",
+        "おもいつき"
+      );
+    }
+    if (id == 2L) {
+      domain = new Domain(
+        2L,
+        "顧客",
+        "-",
+        "顧客の情報を格納する",
+        "いる！"
+      );
+    }
+
+    if (domain != null) {
+      return ResponseEntity.ok().body(domain);
+    }
+    return ResponseEntity.notFound().build();
   }
 }
