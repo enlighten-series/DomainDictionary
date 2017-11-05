7b3e8dd2c9c1614cfba05c6c23a437be88cf5996
佐伯紘二 <saikou9901@gmail.com>
2017-11-05 11:36:31 +0900

    ドメイン編集画面をform化

--- webapp/app/domain-edit-form/domain-edit-form.component.ts
+++ webapp/app/domain-edit-form/domain-edit-form.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Observable } from 'rxjs/Observable';
+
+import { Domain } from '../models/domain';
 
 @Component({
   selector: 'app-domain-edit-form',
@@ -9,13 +13,32 @@ export class DomainEditFormComponent implements OnInit {
 
   @Output() regist = new EventEmitter();
 
+  editingDomain: any = {
+    name: '',
+    format: '',
+    description: '',
+    existential: '',
+  };
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  clickedRegist() {
-    this.regist.emit();
+  clickedRegist(form: NgForm) {
+    if (!form.valid) {
+      // 入力項目をdirtyにしてエラーメッセージを表示する
+      Observable.from(Object.keys(form.controls))
+      .subscribe(
+        key => {
+          form.controls[key].markAsDirty();
+        }
+      )
+      return;
+    }
+
+    console.log(this.editingDomain);
+    this.regist.emit(this.editingDomain);
   }
 
 }
