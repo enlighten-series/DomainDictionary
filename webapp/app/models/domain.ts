3e0ffb368ae92b85560be4f876ed1c72444684de
佐伯紘二 <saikou9901@gmail.com>
2017-11-14 21:42:33 +0900

    作成日時、更新日時を画面に表示

--- webapp/app/models/domain.ts
+++ webapp/app/models/domain.ts
@@ -4,6 +4,8 @@ export class Domain {
   format: string;
   description: string;
   existential: string;
+  created: Date;
+  updated: Date;
 
   constructor(
     id?,
@@ -11,11 +13,15 @@ export class Domain {
     format?,
     description?,
     existential?,
+    created?,
+    updated?,
   ) {
     this.id = id ? id : 1;
     this.name = name ? name : '';
     this.format = format ? format : '';
     this.description = description ? description : '';
     this.existential = existential ? existential : '';
+    this.created = created ? created : new Date();
+    this.updated = updated ? updated : new Date();
   }
 }
