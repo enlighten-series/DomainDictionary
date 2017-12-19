19f61ca4e41d919b81f854796fc821b58a661957
佐伯紘二 <saikou9901@gmail.com>
2017-12-19 20:29:02 +0900

    モデル層：DomainToRelationの取得処理追加

--- src/main/java/org/enlightenseries/DomainDictionary/domain/model/relation/DomainToRelationRepository.java
+++ src/main/java/org/enlightenseries/DomainDictionary/domain/model/relation/DomainToRelationRepository.java
@@ -1,6 +1,10 @@
 package org.enlightenseries.DomainDictionary.domain.model.relation;
 
+import java.util.List;
+
 public interface DomainToRelationRepository {
+
+  List<DomainToRelation> selectDestinations(Long sourceDomainId);
 
   void createTable();
 }
--- src/main/java/org/enlightenseries/DomainDictionary/infrastructure/datasource/relation/DomainToRelationDatasource.java
+++ src/main/java/org/enlightenseries/DomainDictionary/infrastructure/datasource/relation/DomainToRelationDatasource.java
@@ -1,7 +1,10 @@
 package org.enlightenseries.DomainDictionary.infrastructure.datasource.relation;
 
+import org.enlightenseries.DomainDictionary.domain.model.relation.DomainToRelation;
 import org.enlightenseries.DomainDictionary.domain.model.relation.DomainToRelationRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public class DomainToRelationDatasource implements DomainToRelationRepository {
@@ -12,6 +15,10 @@ public class DomainToRelationDatasource implements DomainToRelationRepository {
     DomainToRelationMapper _domainToRelationMapper
   ) {
     this.domainToRelationMapper = _domainToRelationMapper;
+  }
+
+  public List<DomainToRelation> selectDestinations(Long sourceDomainId) {
+    return this.domainToRelationMapper.selectDest(sourceDomainId);
   }
 
   public void createTable() {
