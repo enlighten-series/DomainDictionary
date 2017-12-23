d76f1be9a2f849d7ba1831b2259c6cb7f5382f4a
佐伯紘二 <saikou9901@gmail.com>
2017-12-23 13:09:02 +0900

    サービス層単体テストプロトタイプ新規作成

--- src/test/java/org/enlightenseries/DomainDictionary/application/service/DomainServiceTest.java
+++ src/test/java/org/enlightenseries/DomainDictionary/application/service/DomainServiceTest.java
@@ -1,26 +1,35 @@
-package org.enlightenseries.DomainDictionary.infrastructure.datasource.domain;
+package org.enlightenseries.DomainDictionary.application.service;
 
 import org.enlightenseries.DomainDictionary.DomainDictionaryApplication;
 import org.enlightenseries.DomainDictionary.domain.model.domain.Domain;
+import org.enlightenseries.DomainDictionary.domain.model.domain.DomainRepository;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = DomainDictionaryApplication.class)
-public class DomainMapperTest {
+public class DomainServiceTest {
 
   @Autowired
-  DomainMapper domainMapper;
+  private DomainService domainService;
 
-  Domain assertData;
+  @MockBean
+  private DomainRepository domainRepository;
+
+  private Domain assertData;
 
   @Before
   public void setup() {
@@ -30,19 +39,19 @@ public class DomainMapperTest {
     assertData.setDescription("説明書きです");
     assertData.setExistential(
       "# こんにちは¥r¥n" +
-      "¥r" +
-      "- 世界" +
-      "`こーど`"
+        "¥r" +
+        "- 世界" +
+        "`こーど`"
     );
     assertData.setCreated(new Date("2016/2/29 23:59:59"));
     assertData.setUpdated(new Date("2017/12/31 12:34:56"));
-
-    this.domainMapper.insert(assertData);
   }
 
   @Test
-  public void selectOne() throws Exception {
-    Domain subject = this.domainMapper.select(1L);
+  public void findOneDomain() throws Exception {
+    when(domainRepository.findBy(1L)).thenReturn(assertData);
+
+    Domain subject = this.domainService.findBy(1L);
 
     assertThat(subject.getId()).isEqualTo(assertData.getId());
     assertThat(subject.getName()).isEqualTo(assertData.getName());
