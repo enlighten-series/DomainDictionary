14f9481e89efff30be2402646305c4222f1f64c6
佐伯紘二 <saikou9901@gmail.com>
2018-01-07 20:01:22 +0900

    エクスポートファイルをダウンロードするAPIを実装

--- src/main/java/org/enlightenseries/DomainDictionary/presentation/rest/ApplicationMigrationResource.java
+++ src/main/java/org/enlightenseries/DomainDictionary/presentation/rest/ApplicationMigrationResource.java
@@ -2,12 +2,19 @@ package org.enlightenseries.DomainDictionary.presentation.rest;
 
 import org.enlightenseries.DomainDictionary.application.usecase.ApplicationMigration;
 import org.enlightenseries.DomainDictionary.presentation.rest.dto.ExportFileGeneratingStatus;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Date;
 
 @RestController
@@ -37,5 +44,21 @@ public class ApplicationMigrationResource {
     applicationMigrationUsecase.generatingExportFile();
 
     return ResponseEntity.ok().build();
+  }
+
+  @GetMapping("/application-migration/export/download")
+  public ResponseEntity<Resource> getSimpleExcel() throws Exception {
+    String exportFilePath = this.applicationMigrationUsecase.getExportFilePath();
+
+    File exportFile = new File(exportFilePath);
+    Path absolutePath = Paths.get(exportFile.getAbsolutePath());
+
+    ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(absolutePath));
+
+    return ResponseEntity.ok()
+      .header("Content-disposition", "attachment;filename=" + exportFile.getName())
+      .contentType(MediaType.parseMediaType("text/csv"))
+      .contentLength(exportFile.length())
+      .body(resource);
   }
 }
