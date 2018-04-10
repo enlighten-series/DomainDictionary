f51920493b6d64bf63861d7229619c88279931e8
佐伯紘二 <saikou9901@gmail.com>
2018-04-10 22:05:40 +0900

    プロパティクラスのBean化漏れ

--- src/main/java/org/enlightenseries/DomainDictionary/application/config/LuceneProperties.java
+++ src/main/java/org/enlightenseries/DomainDictionary/application/config/LuceneProperties.java
@@ -3,7 +3,9 @@ package org.enlightenseries.DomainDictionary.application.config;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
+@Component
 @ConfigurationProperties(prefix = "lucene")
 public class LuceneProperties {
 
